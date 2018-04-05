8fd682a1028040ac04e724b2e471ad0544452036
J. Max Wilson <jmaxwilson@gmail.com>
2018-04-05 13:33:54 -0600

    Use the official Docker CE repository instead of docker.io

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -44,9 +44,20 @@ grep -q -F 'bashrc_wsl' ~/.bashrc || echo 'source ~/.bashrc_wsl' >> ~/.bashrc
 echo -e "${GREEN}"
 echo 'Installing and Configuring Docker ...'
 echo -e "${NOCOLOR}"
-sudo apt -q install -y docker-compose putty
-# we have to install the old version of docker.io or it will fail to connect without TLS
-sudo apt-get install docker.io=1.10.3-0ubuntu6 
+sudo apt-get update
+# Remove any existing legacy docker installations
+sudo apt-get remove docker docker-engine docker.io
+# Make sure we can connect to a repository over https
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+# Add the official Docker Community Edition repository
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+# Install Docker CE, docker-compose, and PuTTY
+sudo apt -q install -y docker-ce docker-compose putty
+# Add the current user to the docker group
+sudo usermod -aG docker $USER
 # Tell docker to connect to the Docker of Windows HOST
 echo "export DOCKER_HOST=tcp://127.0.0.1:2375" > ~/.bashrc_wsl
 # create symlink /c to mounted windows c drive so that paths will work
