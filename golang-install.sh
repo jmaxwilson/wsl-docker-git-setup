7485835e8a415499f7f0e584a06a3a39bea36f87
J. Max Wilson <jmaxwilson@gmail.com>
2021-08-26 21:58:23 -0600

    golang-install.sh to wget go 1.17

--- golang-install.sh
+++ golang-install.sh
@@ -5,6 +5,8 @@
 # INIT .BASHRC_WSL
 ./bashrc_wsl.sh
 
-sudo apt install -y golang
+wget https://golang.org/dl/go1.17.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz
+rm go1.17.linux-amd64.tar.gz
 grep -q -F '# GOLANG' ~/.bashrc_wsl || printf "\n# GOLANG\n" >>  ~/.bashrc_wsl
-grep -q -F 'export GOPATH' ~/.bashrc_wsl || echo 'export GOPATH=~/go' >> ~/.bashrc_wsl
+grep -q -F '/usr/local/go/bin' ~/.bashrc_wsl || echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc_wsl
